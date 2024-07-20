939e0d260c398f550b785c5459e5ef2e497993c1
Suhani Kumari <129853575+Suhanisharma19@users.noreply.github.com>
2024-07-20 22:29:15 +0530

    Time: 135 ms (74.61%), Space: 41.4 MB (54.08%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+       int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#'; 
+
+        boolean found = dfs(board, word, i + 1, j, index + 1)
+                || dfs(board, word, i - 1, j, index + 1)
+                || dfs(board, word, i, j + 1, index + 1)
+                || dfs(board, word, i, j - 1, index + 1);
+
+        board[i][j] = temp;  
+
+        return found; 
+    }
+}
