c7306a68c71cc2647e2172285e2a5098e9774fd5
Suhani Kumari <129853575+Suhanisharma19@users.noreply.github.com>
2024-07-20 22:19:04 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (31.06%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+     int m = board.length;
+        int n = board[0].length;
+        int[][] directions = {
+            {0, 1}, {1, 0}, {0, -1}, {-1, 0},
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+        };
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbors = 0;
+                for (int[] dir : directions) {
+                    int ni = i + dir[0];
+                    int nj = j + dir[1];
+
+                    if (ni >= 0 && ni < m && nj >= 0 && nj < n && (board[ni][nj] == 1 || board[ni][nj] == 2)) {
+                        liveNeighbors++;
+                    }
+                }
+                if (board[i][j] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3) {
+                        board[i][j] = 2;  
+                    }
+                } else {
+                    if (liveNeighbors == 3) {
+                        board[i][j] = 3;  
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 2) {
+                    board[i][j] = 0;  
+                } else if (board[i][j] == 3) {
+                    board[i][j] = 1;  
+                }
+            }
+        }
+ 
+
+ 
+    }
+}
